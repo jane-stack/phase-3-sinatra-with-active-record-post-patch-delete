c96405f179cc6e086e608422ea2b13d60d4ad487
Tuyen Ha <tuyen_ha@outlook.com>
2023-01-18 12:30:07 -0700

    commit tree 657a6ffa2ef96f0b4e2991c0344e1142fa8b2090

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,43 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  patch '/reviews/:id' do
+    # find the review to update using the ID
+    review = Review.find(params[:id])
+    # Access the data in the body of the request
+    review.update(
+      # use that data to update the review in the database
+      score: params[:score],
+      comment: params[:comment]
+    )
+    # Send a response with updated review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+  # find the review to delete using the ID
+    review = Review.find(params[:id])
+    # Delete the review from the database
+    review.destroy
+    # Send a response with the deleted review as JSON to confirm that it was deleted successfully, so the frontend can show the successful deletion to the use.
+    review.to_json
+  end
+
+  get '/reviews' do
+    review = Review.all
+    review.to_json
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
